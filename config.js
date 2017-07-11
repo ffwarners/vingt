63c753a20121ac649cb58872e5f0784026324365
Steven <stevenlambregts@gmail.com>
2017-07-11 14:28:27 +0200

    Connection established

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ config.PORT = 3000;
 config.dbData = {
     host: 'localhost',
     port: 3306,
-    user: 'root',
-    password: '',
-    database: ''
+    user: 'Wijn',
+    password: 'Vingt',
+    database: 'wines'
 };
 
 module.exports = config;
